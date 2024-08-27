59e6a5d95b6b015d57008146fda383077ec738ae
harshhelaiya <harsh.helaiya@teknopoint.in>
2024-08-27 23:09:30 +0530

    assingment

---
+++ index.js
@@ -0,0 +1,25 @@
+// const jwt = require("jsonwebtoken");
+
+// const value = {
+//     fullName: "Harsh Helaiya",
+//     accountNumber: 52515251
+// }
+
+// const jwtToken = jwt.sign(value, '123');
+// console.log(jwtToken);
+
+// // OUTPUT
+// const output = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmdWxsTmFtZSI6IkhhcnNoIEhlbGFpeWEiLCJhY2NvdW50TnVtYmVyIjo1MjUxNTI1MSwiaWF0IjoxNzI0Nzc1OTA5fQ.j3YcIA7EejjQmr1rPCSc5FDlPw__HBWv7LDTL3FuTRo"
+
+
+// const jwtDecode = jwt.decode(output,'1234');
+// console.log(jwtDecode);
+
+let a;
+try {
+    console.log(a.length);
+    console.log('sdkfasfd');
+} catch (error) {
+    console.log(error);
+    console.log('hi rhere');    
+}
