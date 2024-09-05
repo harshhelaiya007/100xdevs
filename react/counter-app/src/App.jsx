7b523e62469dad0f10212d4d964f89ce019cb352
harshhelaiya <harsh.helaiya@teknopoint.in>
2024-09-05 08:19:33 +0530

    wip

--- react/counter-app/src/App.jsx
+++ react/counter-app/src/App.jsx
@@ -3,22 +3,49 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [todo, setTodo] = useState([
+    {
+      title: "first Todod",
+      description: "lorem ipsusm",
+    },
+    {
+      title: "second",
+      description: "lorem ipsusm",
+    },
+  ]);
 
   return (
     <div>
-      <CustomButton count={count} setCount={setCount}/>
+      <CustomButton count={count} setCount={setCount} />
+      <TodoFn todo={todo} />
     </div>
   );
 }
 
 function CustomButton(props) {
+  return (
+    <button
+      onClick={() => {
+        props.setCount((count) => count + 1);
+      }}
+    >
+      Counter {props.count}
+    </button>
+  );
+}
 
-  return <button onClick={()=>{
-    props.setCount((count) => count + 1)
-  }}>
-    Counter {props.count}
-  </button>
-
+function TodoFn({ todo }) {
+  console.log(todo); // This will now correctly log the todo array
+  return (
+    <ul>
+      {todo.map((item, index) => (
+        <li key={index}>
+          {item.title}
+          <p>{item.description}</p>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default App;
