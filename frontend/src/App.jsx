352395b92a55092beb9977b54b8f86abeffcce10
harshhelaiya <harsh.helaiya@teknopoint.in>
2024-09-07 15:51:10 +0530

    work in progress

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,30 @@ export default function App() {
   return (
     <>
       <div className="input-wrapper">
-        <h2 style={{textAlign: "center", padding:"10px 0"}}>Sign Up</h2>
-        <Input />
-        <br/>
-        <Input />
-        <button>Sign Up</button>
+        <h2 style={{ textAlign: "center", padding: "10px 0" }}>Sign Up</h2>
+        <Input inputId="userName" inputLabel="Username" />
+        <br />
+        <Input inputId="email" inputLabel="Email" />
+        <br />
+        <Input inputId="password" inputLabel="Password" />
+        <button
+          className="button"
+          onClick={() => {
+            fetch("http://localhost:3000/user/signup", {
+              method: "POST",
+              headers: {
+                "content-type": "application/json",
+              },
+              body: JSON.stringify({
+                username: "harshhelaiya",
+                email: "harshhelaiya4@gmail.com",
+                password: "Justin@007",
+              }),
+            });
+          }}
+        >
+          Sign Up
+        </button>
       </div>
     </>
   );
--- frontend/src/Components/Input/Input.jsx
+++ frontend/src/Components/Input/Input.jsx
@@ -1,9 +1,14 @@
 import "./Input.css";
 
-export default function Input() {
-    return(
-        <div className="input-div">
-            <input type="text" />
-        </div>
-    )
+export default function Input({
+  inputId = "",
+  inputType = "text",
+  inputLabel = "Label",
+}) {
+  return (
+    <div className="input-div">
+      <label htmlFor={inputId}>{inputLabel}</label>
+      <input type={inputType} id={inputId} />
+    </div>
+  );
 }
