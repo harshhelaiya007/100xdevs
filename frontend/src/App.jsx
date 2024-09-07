a53eaeaf6d00d6d1cd07d476da7508d39ddd2d33
harshhelaiya <harsh.helaiya@teknopoint.in>
2024-09-07 15:33:38 +0530

    frotnend dev

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
 import Input from "./Components/Input";
+import "./App.css";
 
 export default function App() {
   return (
     <>
-      <Input />
+      <div className="input-wrapper">
+        <h2 style={{textAlign: "center", padding:"10px 0"}}>Sign Up</h2>
+        <Input />
+        <br/>
+        <Input />
+        <button>Sign Up</button>
+      </div>
     </>
   );
 }
