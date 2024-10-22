06a075d8ed9720f6035ad8130de08faaef5fb489
harshhelaiya <harsh.helaiya@teknopoint.in>
2024-10-22 13:33:06 +0530

    done

--- routing-learn/src/App.jsx
+++ routing-learn/src/App.jsx
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
-import Dashboard from "./components/Dashboard/Dashboard";
-import Landing from "./components/Landing/Landing";
+// import Dashboard from "./components/Dashboard/Dashboard";
+// import Landing from "./components/Landing/Landing";
+const Dashboard = React.lazy(() => import("./components/Dashboard/Dashboard"));
+const Landing = React.lazy(() => import("./components/Landing/Landing"));
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <AppBar />
+        <Suspense fallback={'Loading'}>
         <Routes>
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/" element={<Landing />} />
         </Routes>
+        </Suspense>
       </BrowserRouter>
     </>
   );
